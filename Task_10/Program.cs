c5b86145d16310bc6111fe9b4287a3fe556f5b65
SaidGadjiev <ackusha_dargo@bk.ru>
2022-12-04 15:29:59 +0300

    add task 10, 11

---
+++ Task_10/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число");
+string number_str = Console.ReadLine() ??" "; // Вводим данные
+int numberLeng = number_str.Length; // проверяем длину строки
+int number;
+bool isNumber = int.TryParse(number_str, out number); // проверяет явл введеные данные только цифры
+
+if (isNumber == true) // проверка данных на соответствие если да то
+{
+    if (numberLeng == 3) // если явл число трехзначным то
+    {
+        
+        void SecondNumber(int number)
+        {
+        Console.WriteLine(number/10%10);
+        }
+    SecondNumber(number);
+    }
+
+    else Console.WriteLine("Вы ввели не трехзначное число попробуйте снова"); // если не явл
+}
+
+else Console.WriteLine("У вас есть сымволы не являюшиеся цифрами, Вводите только цифры"); // имеются символы не явл цифрами
+
---
+++ Task_11/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите любое число - ");
+string number_str = Console.ReadLine()??" ";
+int numberLeng = number_str.Length;
+if (numberLeng < 3)
+Console.WriteLine(numberLeng);
