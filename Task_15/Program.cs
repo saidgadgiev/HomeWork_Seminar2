545dfd0115edbe3878d0aabbfca91f292b78e646
saidgadgiev <117773666+saidgadgiev@users.noreply.github.com>
2022-12-04 21:40:26 +0300

    add modified Task 15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введити цифру от 1 до 7 - ");
+string numStr = Console.ReadLine()??" ";
+Console.WriteLine("");
+int num = Convert.ToInt32(numStr);
+string [] mass = {"Понидельник", "Вторник", "Среда", 
+                "Четверг", "Пятница", "Суббота", "воскресенье"};
+if (num < 8 && num > 0)
+{
+    if (num < 6)
+    {
+        Console.WriteLine($"Сегодня {num} день недели. Это {mass[num-1]} значит РАБОЧИЙ день");
+    }
+    else Console.WriteLine($"Сегодня {num} день недели. Это {mass[num-1]} значит ВЫХОДНОЙ");
+}
+else 
+{
+    Console.WriteLine("Вы ввели не допустимую цифру");
+    Console.WriteLine("Введити цифру от 1 до 7");
+}
+
+
+
