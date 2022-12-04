ae6110a5ace82f123b6ff53a39d0525ba6a5e960
saidgadgiev <117773666+saidgadgiev@users.noreply.github.com>
2022-12-04 21:46:12 +0300

    add modified Task 15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -10,22 +10,26 @@
 Console.Write("Введити цифру от 1 до 7 - ");
 string numStr = Console.ReadLine()??" ";
 Console.WriteLine("");
-int num = Convert.ToInt32(numStr);
+//int num = Convert.ToInt32(numStr);
 string [] mass = {"Понидельник", "Вторник", "Среда", 
                 "Четверг", "Пятница", "Суббота", "воскресенье"};
-if (num < 8 && num > 0)
-{
-    if (num < 6)
+int num;
+bool isNum = int.TryParse(numStr, out num);
+if (isNum == true)
+{ 
+    if (num < 8 && num > 0)
     {
-        Console.WriteLine($"Сегодня {num} день недели. Это {mass[num-1]} значит РАБОЧИЙ день");
+        if (num < 6)
+        {
+            Console.WriteLine($"Сегодня {num} день недели. Это {mass[num-1]} значит РАБОЧИЙ день");
+        }
+        else Console.WriteLine($"Сегодня {num} день недели. Это {mass[num-1]} значит ВЫХОДНОЙ");
     }
-    else Console.WriteLine($"Сегодня {num} день недели. Это {mass[num-1]} значит ВЫХОДНОЙ");
+    else 
+    {
+        Console.WriteLine("Вы ввели не допустимую цифру");
+        Console.WriteLine("Введити цифру от 1 до 7");
+    }
 }
-else 
-{
-    Console.WriteLine("Вы ввели не допустимую цифру");
-    Console.WriteLine("Введити цифру от 1 до 7");
-}
-
-
+Console.WriteLine("Вы ввели не цифру. Введити цифру от 1 до 7");
 
